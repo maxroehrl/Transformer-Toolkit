ce9b10329ed73c152008193666d9099b07870604
Max Röhrl <max.roehrl11@gmail.com>
2016-04-06 15:56:11 +0200

    Bump version to 4.1.1

--- Toolkit/Properties/AssemblyInfo.cs
+++ Toolkit/Properties/AssemblyInfo.cs
@@ -36,6 +36,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.1.0")]
-[assembly: AssemblyFileVersion("4.1.0")]
+[assembly: AssemblyVersion("4.1.1")]
+[assembly: AssemblyFileVersion("4.1.1")]
 [assembly: NeutralResourcesLanguage("en")]
